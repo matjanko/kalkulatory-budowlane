720a044911802564a32d2edfc7c245a446890013
matjanko <mateusz.jankowski@prokonto.pl>
2020-09-17 22:17:25 +0200

    Added sheets component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { SheetsComponent } from './pages/sheets/sheets.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+  },
+  {
+    path: 'kalkulatory',
+    component: SheetsComponent,
   },
 ];
 
