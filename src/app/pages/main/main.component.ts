9bf19124478c8270bd2233739539aa12eed9cb2a
matjanko <mateusz.jankowski@prokonto.pl>
2020-09-26 14:19:50 +0200

    Added router subscription in the main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Sheet } from '../../shared/models/sheet-list/sheet';
 import { SheetService } from '../../shared/services/sheet.service';
+import { NavigationEnd, Router} from '@angular/router';
+import {filter, map} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.less'],
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-  sheetName: string;
   sheets = new Array<Sheet>();
+  selectedSheet: Sheet;
 
-  constructor(private sheetService: SheetService) {}
+  private subscription = new Subscription();
+
+  constructor(
+    private router: Router,
+    private sheetService: SheetService
+  ) {}
 
   ngOnInit(): void {
     this.sheets = this.sheetService.getSheets();
+    this.selectedSheet = this.getSheetByPath(this.router.url);
+
+    this.subscription.add(this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event as NavigationEnd)
+    ).subscribe(event => {
+      this.selectedSheet = this.getSheetByPath(event.url);
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private getSheetByPath(path: string): Sheet {
+    return this.sheets.find(s => s.path === path);
   }
 }
